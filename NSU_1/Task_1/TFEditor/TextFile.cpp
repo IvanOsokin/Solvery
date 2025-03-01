aff9396c9f48437c54791c27ddf4ec9fd9720d25
IvanOsokin <osokin.ivan.nstu@gmail.com>
2025-03-02 00:21:27 +0700

    Remove unused files from TFEditor

--- NSU_1/Task_1/TFEditor/TextFile.cpp
+++
@@ -1,9 +0,0 @@
-#include "TextFile.h"
-
-/*TextFile::TextFile(const fs::path& fileName)
-{
-	if (!fs::exists(fileName))
-		throw std::runtime_error(std::string("File \"" + fileName.string() + "\" does not found!"));
-
-	;
-}*/
