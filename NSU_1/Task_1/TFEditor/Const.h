88605102744794e49b60c2dbf86e09983f906a93
IvanOsokin <osokin.ivan.nstu@gmail.com>
2025-03-03 23:43:23 +0700

    Refactor (add constants)

---
+++ NSU_1/Task_1/TFEditor/Const.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace TFEConst
+{
+	namespace Errors
+	{
+		const int INVALID_CMDLINE_ARGS_CNT = 1;
+		const int SRC_FILE_NOT_EXITS	   = 2;
+		const int INVALID_OUTPUT_FILENAME  = 3;
+		const int INVALID_OUTPUT_DIRECTORY = 4;
+	}
+
+	namespace Arguments
+	{
+		const int CMD_IDX_FILENAME	  = 0;
+		const int CMD_IDX_INPUT_FILE  = 1;
+		const int CMD_IDX_OUTPUT_FILE = 2;
+		const int CMD_IDX_OLD_PATTERN = 3;
+		const int CMD_IDX_NEW_PATTERN = 4;
+	}
+
+	namespace Messages
+	{
+
+	}
+}
--- NSU_1/Task_1/TFEditor/main.cpp
+++ NSU_1/Task_1/TFEditor/main.cpp
@@ -1,28 +1,28 @@
 #include "pch.h"
 
 #include "TextEditor.h"
-#include "Errors.h"
+#include "Const.h"
 
 int main(int argc, char* argv[])
 {
 	if (5 != argc)
 	{
 		std::cerr << "Error: invalid number of the command line parameters: expect 5, actually " << argc << ".";
-		return TFEError::INVALID_CMDLINE_ARGS_CNT;
+		return TFEConst::Errors::INVALID_CMDLINE_ARGS_CNT;
 	}
 
-	std::filesystem::path inputFile = std::filesystem::path(argv[1]);
+	std::filesystem::path inputFile = std::filesystem::path(argv[TFEConst::Arguments::CMD_IDX_INPUT_FILE]);
 	if (!std::filesystem::exists(inputFile))
 	{
 		std::cerr << "Error: the input file \"" << inputFile.string() << "\" does not exist on the drive \"" << inputFile.root_path().string() << "\".";
-		return TFEError::SRC_FILE_NOT_EXITS;
+		return TFEConst::Errors::SRC_FILE_NOT_EXITS;
 	}
 
-	std::filesystem::path outputFile = std::filesystem::path(argv[2]);
+	std::filesystem::path outputFile = std::filesystem::path(argv[TFEConst::Arguments::CMD_IDX_OUTPUT_FILE]);
 	if (!outputFile.has_filename() || !outputFile.has_extension())
 	{
 		std::cerr << "Error: the specified output file name is invalid!";
-		return TFEError::INVALID_OUTPUT_FILENAME;
+		return TFEConst::Errors::INVALID_OUTPUT_FILENAME;
 	}
 
 	if (outputFile.parent_path().empty())
@@ -32,18 +32,18 @@ int main(int argc, char* argv[])
 	else if (!std::filesystem::exists(outputFile.parent_path()))
 	{
 		std::cerr << "Error: he directory for the output file \"" << outputFile.parent_path().string() << "\" does not exist!";
-		return TFEError::INVALID_OUTPUT_DIRECTORY;
+		return TFEConst::Errors::INVALID_OUTPUT_DIRECTORY;
 	}
 
 	try
 	{
 		TextEditor textEditor(inputFile, outputFile);
-		textEditor.ReplaceSubstring(argv[3], argv[4]);
-		return TFEError::SUCCESS;
+		textEditor.ReplaceSubstring(argv[TFEConst::Arguments::CMD_IDX_OLD_PATTERN], argv[TFEConst::Arguments::CMD_IDX_NEW_PATTERN]);
+		return EXIT_SUCCESS;
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << "An error occured during process: " << e.what();
-		return TFEError::APP_RUNTIME_ERROR;
+		return EXIT_FAILURE;
 	}
 }
